5ba0cee60e9cc7a8a57d7fb30a56f3cf1679af18
AI509-17 <sjh980313@gmail.copm>
2024-08-27 15:37:45 +0900

    0827-첫강의

---
+++ src/main/java/kr/ac/kopo/board/entity/Board.java
@@ -0,0 +1,23 @@
+package kr.ac.kopo.board.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.*;
+
+@Entity
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@ToString
+
+public class Board extends BaseEntity{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // 1씩 자동 증가
+    private Long bno;
+
+    private String title;
+    private String content;
+}
